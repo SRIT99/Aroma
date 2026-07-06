db0bfd7367388665e90df75ff4b8fc8a93be0e92
Suraj <surajworkspace12@gmail.com>
2026-07-06 23:45:53 +0545

    Creating an optimized production build ... ✓ Compiled successfully in 9.6s ✓ Finished TypeScript in 6.4s ✓ Collecting page data using 6 workers in 1674ms ✓ Generating static pages using 6 workers (5/5) in 1735ms ✓ Finalizing page optimization in 36ms

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -74,17 +74,7 @@ const nextConfig = {
     ];
   },
 
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
+  turbopack:{}
 };
 
 module.exports = nextConfig;
